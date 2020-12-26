ec9a9453e520502fc2d0dd65c89a5d485cc51da5
Evan Klein <evanrklein@gmail.com>
2020-12-26 19:36:19 +0000

    Bot can mute, deafen, unmute, and undeafen specific users.

--- main.py
+++ main.py
@@ -1,22 +1,38 @@
-import Discord
+import discord
 import os
 import random
 
+def mute(member, muted=True):
+  await member.edit(mute=muted)
 
-client = Discord.Client()
+def deafen(member, deafened=True):
+  await member.edit(deafen=deafened)
+
+client = discord.Client()
 
 @client.event
 async def on_ready():
   print("logged in as {0.user}".format(client))
 
 @client.event
-async def on_message():
+async def on_message(message):
   if message.author == client.user:
     return
   
-  if message.startswith('$mute'):
-    while run == True:
-    time.sleep(random.randint())      
+  if message.content.startswith('%mute'):
+    member = message.mentions[0]
+    await member.edit(mute=True)
+  
+  if message.content.startswith('%deafen'):
+    member = message.mentions[0]
+    await member.edit(deafen=True)
 
+  if message.content.startswith('%unmute'):
+    member = message.mentions[0]
+    await member.edit(mute=False)
+  
+  if message.content.startswith('%undeafen'):
+    member = message.mentions[0]
+    await member.edit(deafen=False)
 
 client.run(os.getenv("TOKEN"))
